a2e19ace74865e9eb4225cb80ea2a0b335136bb7
AnnikaH <annika_limfe@hotmail.com>
2015-10-25 16:45:50 +0100

    Settings and actionbar options menu option added and actionbar back on front page

--- app/src/main/java/com/example/annika/wishlist/SettingsActivity.java
+++ app/src/main/java/com/example/annika/wishlist/SettingsActivity.java
@@ -1,17 +1,16 @@
 package com.example.annika.wishlist;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class TipsFriendsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tips_friends);
+        setContentView(R.layout.activity_settings);
     }
 
     @Override
@@ -23,12 +22,13 @@ public class TipsFriendsActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+        //noinspection SimplifiableIfStatement
         if (id == R.id.settings) {
-            Intent i = new Intent(this, SettingsActivity.class);
-            startActivity(i);
-            finish();
             return true;
         }
 
--- app/src/main/java/com/example/annika/wishlist/WishListMainActivity.java
+++ app/src/main/java/com/example/annika/wishlist/WishListMainActivity.java
@@ -38,4 +38,24 @@ public class WishListMainActivity extends AppCompatActivity {
         Intent i = new Intent(this, FriendsWishListsActivity.class);
         startActivity(i);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_wish_list_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.settings) {
+            Intent i = new Intent(this, SettingsActivity.class);
+            startActivity(i);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
