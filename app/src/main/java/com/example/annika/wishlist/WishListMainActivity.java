d60399f139e224dd33439c4f5d25464df927182e
AnnikaH <annika_limfe@hotmail.com>
2015-10-25 16:30:52 +0100

    Activities and layout portrait for four buttons on front page created

--- app/src/main/java/com/example/annika/wishlist/WishListMainActivity.java
+++ app/src/main/java/com/example/annika/wishlist/WishListMainActivity.java
@@ -1,9 +1,11 @@
 package com.example.annika.wishlist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class WishListMainActivity extends AppCompatActivity {
 
@@ -13,25 +15,27 @@ public class WishListMainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_wish_list_main);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_wish_list_main, menu);
-        return true;
+    // OnClick profile_button:
+    public void profileClicked(View view) {
+        Intent i = new Intent(this, ProfileActivity.class);
+        startActivity(i);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    // OnClick my_wish_lists_button:
+    public void myWishListsClicked(View view) {
+        Intent i = new Intent(this, MyWishListsActivity.class);
+        startActivity(i);
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+    // OnClick tips_friends_button:
+    public void tipsFriendsClicked(View view) {
+        Intent i = new Intent(this, TipsFriendsActivity.class);
+        startActivity(i);
+    }
 
-        return super.onOptionsItemSelected(item);
+    // OnClick friends_wish_lists_button:
+    public void friendsWishListsClicked(View view) {
+        Intent i = new Intent(this, FriendsWishListsActivity.class);
+        startActivity(i);
     }
 }
