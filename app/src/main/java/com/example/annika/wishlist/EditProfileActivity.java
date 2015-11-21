406c4a9e5e96a23c37813d81ec50c77712811ccf
AnnikaH <annika_limfe@hotmail.com>
2015-11-21 04:31:24 +0100

    Edit activity

---
+++ app/src/main/java/com/example/annika/wishlist/EditProfileActivity.java
@@ -0,0 +1,48 @@
+package com.example.annika.wishlist;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+
+    // Onclick save-button:
+    public void saveChanges(View view)
+    {
+        // save changes
+
+        Intent i = new Intent(this, ProfileActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_wish_list_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.settings) {
+            Intent i = new Intent(this, SettingsActivity.class);
+            startActivity(i);
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
