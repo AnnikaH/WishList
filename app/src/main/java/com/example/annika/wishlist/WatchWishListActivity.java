949b48823b254f881264b747033e587a92bce34a
AnnikaH <annika_limfe@hotmail.com>
2015-11-24 21:17:42 +0100

    Working on friends wish lists

--- app/src/main/java/com/example/annika/wishlist/WatchWishListActivity.java
+++ app/src/main/java/com/example/annika/wishlist/WatchWishListActivity.java
@@ -5,6 +5,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+import retrofit.mime.TypedByteArray;
 
 public class WatchWishListActivity extends AppCompatActivity {
 
@@ -28,10 +39,40 @@ public class WatchWishListActivity extends AppCompatActivity {
         restWishService = new RestWishService();
 
         // get the user name of the owner of the wish list:
+        restUserService.getService().getUserById(ownerId, new Callback<Response>() {
+            @Override
+            public void success(Response response, Response response2) {
+                try {
+                    JSONObject userObject = new JSONObject(new String(((TypedByteArray) response.getBody()).getBytes()));
+                    ownerUserName = userObject.getString("userName");
+                } catch (JSONException je) {
+                    Toast toast = Toast.makeText(WatchWishListActivity.this,
+                            getApplicationContext().getString(R.string.json_exception),
+                            Toast.LENGTH_SHORT);
+                    View toastView = toast.getView();
+                    toastView.setBackgroundResource(R.color.background_color);
+                    toast.show();
+                }
+            }
 
+            @Override
+            public void failure(RetrofitError error) {
+                Toast toast = Toast.makeText(WatchWishListActivity.this,
+                        getApplicationContext().getString(R.string.get_user_error_message),
+                        Toast.LENGTH_SHORT);
+                View toastView = toast.getView();
+                toastView.setBackgroundResource(R.color.background_color);
+                toast.show();
+            }
+        });
 
-        // get all wishes for this wishlist and show them in the ListView:
+        // setText in TextView: wishListName and ownerUserName
+        TextView headerTextView = (TextView) findViewById(R.id.wishListNameHeader);
+        headerTextView.setText(ownerUserName + ": " + wishListName);
+
+        // TODO: get all wishes for this wishlist and show them in the ListView (friendsWishesListView):
         // onclick each wish: see details for the wish
+
 
     }
 
