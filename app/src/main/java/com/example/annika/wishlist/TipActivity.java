56c3f6262aa13f4c0cfbbb0ed383755fea87d0ec
AnnikaH <annika_limfe@hotmail.com>
2015-11-25 22:10:10 +0100

    Tip finished

--- app/src/main/java/com/example/annika/wishlist/TipActivity.java
+++ app/src/main/java/com/example/annika/wishlist/TipActivity.java
@@ -6,11 +6,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+import retrofit.mime.TypedByteArray;
 
 public class TipActivity extends AppCompatActivity {
 
     private int tipId;
     private int userId;
+    RestTipService restTipService;
+    RestUserService restUserService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +33,90 @@ public class TipActivity extends AppCompatActivity {
         tipId = getIntent().getIntExtra("TIPID", -1);
         userId = getIntent().getIntExtra("USERID", -1);
 
-        // get the tip from the database and show the tip:
+        restTipService = new RestTipService();
+        restUserService = new RestUserService();
 
+        // get the tip from the database and show the tip and the user name to the sender:
+        restTipService.getService().getTipById(tipId, new Callback<Response>() {
+            @Override
+            public void success(Response response, Response response2) {
+                try {
+                    JSONObject tip = new JSONObject(new String(((TypedByteArray) response.getBody()).getBytes()));
+
+                    String tipName = tip.getString("name");
+                    String tipSpesification = tip.getString("spesification");
+                    double tipPrice = tip.getDouble("price");
+                    String tipLink = tip.getString("link");
+                    String tipWhere = tip.getString("where");
+                    // TODO: image
+                    int tipSenderId = tip.getInt("senderId");
+                    // int tipReceiverId = tip.getInt("receiverId");
+
+                    TextView nameText = (TextView) findViewById(R.id.tipName);
+                    TextView spesText = (TextView) findViewById(R.id.tipSpesification);
+                    TextView whereText = (TextView) findViewById(R.id.tipWhere);
+                    TextView linkText = (TextView) findViewById(R.id.tipLink);
+                    TextView priceText = (TextView) findViewById(R.id.tipPrice);
+
+                    nameText.setText(tipName);
+                    spesText.setText(tipSpesification);
+                    whereText.setText(tipWhere);
+                    linkText.setText(tipLink);
+                    priceText.setText(tipPrice + " kr");
+
+                    showSender(tipSenderId);
+
+                } catch (JSONException je) {
+                    Toast toast = Toast.makeText(TipActivity.this,
+                            getApplicationContext().getString(R.string.json_exception),
+                            Toast.LENGTH_SHORT);
+                    View toastView = toast.getView();
+                    toastView.setBackgroundResource(R.color.background_color);
+                    toast.show();
+                }
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Toast toast = Toast.makeText(TipActivity.this,
+                        getApplicationContext().getString(R.string.get_tip_error_message),
+                        Toast.LENGTH_LONG);
+                View toastView = toast.getView();
+                toastView.setBackgroundResource(R.color.background_color);
+                toast.show();
+            }
+        });
+    }
+
+    public void showSender(int senderId) {
+        // find user name to sender with senderId and show it in the TextView
+        restUserService.getService().getUserById(senderId, new Callback<Response>() {
+            @Override
+            public void success(Response response, Response response2) {
+                try {
+                    JSONObject user = new JSONObject(new String(((TypedByteArray) response.getBody()).getBytes()));
+                    TextView senderText = (TextView) findViewById(R.id.tipSender);
+                    senderText.setText(user.getString("userName"));
+                } catch (JSONException je) {
+                    Toast toast = Toast.makeText(TipActivity.this,
+                            getApplicationContext().getString(R.string.json_exception),
+                            Toast.LENGTH_SHORT);
+                    View toastView = toast.getView();
+                    toastView.setBackgroundResource(R.color.background_color);
+                    toast.show();
+                }
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Toast toast = Toast.makeText(TipActivity.this,
+                        getApplicationContext().getString(R.string.get_user_error_message),
+                        Toast.LENGTH_LONG);
+                View toastView = toast.getView();
+                toastView.setBackgroundResource(R.color.background_color);
+                toast.show();
+            }
+        });
     }
 
     // Onclick back-button
