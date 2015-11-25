f81f81f4d22fd7be9d344fa01f2fb9e14d722e38
AnnikaH <annika_limfe@hotmail.com>
2015-11-25 20:21:22 +0100

    New tip registered

--- app/src/main/java/com/example/annika/wishlist/NewTipActivity.java
+++ app/src/main/java/com/example/annika/wishlist/NewTipActivity.java
@@ -26,6 +26,7 @@ public class NewTipActivity extends AppCompatActivity {
     private RestTipService restTipService;
     private User foundUser;
     private Button userNameCheckButton;
+    private boolean registerButtonPushed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,12 +40,13 @@ public class NewTipActivity extends AppCompatActivity {
 
         userNameCheckButton = (Button) findViewById(R.id.userNameCheckButton);
 
+        registerButtonPushed = false;
+
 
     }
 
     // Onclick search-button
-    public void searchForFriendByUserName(View view) {
-        // Toast only if found friend
+    public void searchForFriendByUserName(final View view) {
 
         EditText userNameEdit = (EditText) findViewById(R.id.userNameEditSearch);
         final String userNameSearched = userNameEdit.getText().toString();
@@ -68,12 +70,18 @@ public class NewTipActivity extends AppCompatActivity {
 
                             userNameCheckButton.setBackgroundResource(R.drawable.checkmark_100);
 
+                            if(registerButtonPushed) {
+                                registerButtonPushed = false;   // reset
+                                registerTipInDatabase(view);
+                            }
+
                             return; // don't need to search any more
                         }
                     }
 
                     // if gets here there was no user with this username:
                     userNameCheckButton.setBackgroundResource(R.drawable.delete_cross_100);
+                    registerButtonPushed = false;
 
                 } catch (JSONException je) {
                     Toast toast = Toast.makeText(NewTipActivity.this,
@@ -82,6 +90,8 @@ public class NewTipActivity extends AppCompatActivity {
                     View toastView = toast.getView();
                     toastView.setBackgroundResource(R.color.background_color);
                     toast.show();
+
+                    registerButtonPushed = false;
                 }
             }
 
@@ -93,18 +103,111 @@ public class NewTipActivity extends AppCompatActivity {
                 View toastView = toast.getView();
                 toastView.setBackgroundResource(R.color.background_color);
                 toast.show();
+
+                registerButtonPushed = false;
             }
         });
     }
 
     // Onclick register-button
     public void registerNewTip(View view) {
+        registerButtonPushed = true;
+        searchForFriendByUserName(view);    // to check the user name that's been written +
+        // store this users values in foundUser
+        // The rest of the code for what should happen if user name valid is written inside
+        // searchForFriendByUserName-method because it's an async call
+    }
 
+    public void registerTipInDatabase(View view) {
+        // register button has been pushed and we have the foundUser values (are valid)
+
+        // Get tip values:
+        EditText tipNameEdit = (EditText) findViewById(R.id.tipName);
+        EditText tipLinkEdit = (EditText) findViewById(R.id.tipLink);
+        EditText tipPriceEdit = (EditText) findViewById(R.id.tipPrice);
+        EditText tipSpesEdit = (EditText) findViewById(R.id.tipSpesification);
+        EditText tipWhereEdit = (EditText) findViewById(R.id.tipWhere);
+        // TODO: ImageView imageView = (ImageView) findViewById(R.id.tipImage);
+
+        String name = tipNameEdit.getText().toString();
+        String link = tipLinkEdit.getText().toString();
+        String priceString = tipPriceEdit.getText().toString();
+        String spes = tipSpesEdit.getText().toString();
+        String where = tipWhereEdit.getText().toString();
+        double price;
+
+        try {
+            price = Double.parseDouble(priceString);
+        } catch (NumberFormatException nfe) {
+            price = 0;
+        }
+
+        // Input validation (not all fields have to be filled in):
+
+        // backend: [RegularExpression("^[a-zæøåA-ZÆØÅ0-9., \\-]{2,30}$")]
+        if(!name.matches("^[a-zæøåA-ZÆØÅ0-9., \\-]{2,30}$")) {
+            tipNameEdit.setError(getString(R.string.name_error_message));
+            return;
+        }
+
+        // backend: not required, but if filled in: [RegularExpression("^[0-9\\.]{0,9}$")]
+        if(!priceString.matches("^[0-9\\.]{0,9}$")) {
+            tipPriceEdit.setError(getString(R.string.price_error_message));
+            return;
+        }
+
+        WishTip tip = new WishTip();
+        tip.Link = link;
+        tip.Where = where;
+        tip.Name = name;
+        tip.Price = price;
+        tip.Spesification = spes;
+        tip.SenderId = userId;
+        tip.ReceiverId = foundUser.ID;
+        // TODO: tip.Image = ...;
+
+        // disable button click (in case it takes some time):
+        final Button button = (Button) view;
+        button.setEnabled(false);
+
+        // post:
+        restTipService.getService().addTip(tip, new Callback<Response>() {
+            @Override
+            public void success(Response response, Response response2) {
+                Toast toast = Toast.makeText(NewTipActivity.this,
+                        getApplicationContext().getString(R.string.tip_registered),
+                        Toast.LENGTH_SHORT);
+                View toastView = toast.getView();
+                toastView.setBackgroundResource(R.color.background_color);
+                toast.show();
+
+                Intent i = new Intent(NewTipActivity.this, TipsFriendsActivity.class);
+                i.putExtra("USERID", userId);
+                startActivity(i);
+                finish();
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Toast toast = Toast.makeText(NewTipActivity.this,
+                        getApplicationContext().getString(R.string.tip_registered_error_message),
+                        Toast.LENGTH_LONG);
+                View toastView = toast.getView();
+                toastView.setBackgroundResource(R.color.background_color);
+                toast.show();
+
+                // enable button again:
+                button.setEnabled(true);
+            }
+        });
     }
 
     // Onclick cancel-button
     public void cancelNewTip(View view) {
-
+        Intent i = new Intent(this, TipsFriendsActivity.class);
+        i.putExtra("USERID", userId);
+        startActivity(i);
+        finish();
     }
 
     @Override
