59b4055137c012208cb04d40633508ff5ce54336
AnnikaH <annika_limfe@hotmail.com>
2015-11-26 06:13:16 +0100

    Layout land

--- app/src/main/java/com/example/annika/wishlist/NewWishActivity.java
+++ app/src/main/java/com/example/annika/wishlist/NewWishActivity.java
@@ -24,31 +24,12 @@ import retrofit.client.Response;
 
 public class NewWishActivity extends AppCompatActivity {
 
-    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
-    private static final int CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE = 200;
-    private Uri fileUri;
-
     private int wishListId;
     private String wishListName;
     private int userId;
     private RestWishService restWishService;
     private Button button;
-/*
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
-                // Image captured and saved to fileUri specified in the Intent
-                Toast.makeText(this, "Image saved to:\n" +
-                        data.getData(), Toast.LENGTH_LONG).show();
-            } else if (resultCode == RESULT_CANCELED) {
-                // User cancelled the image capture
-            } else {
-                // Image capture failed, advise user
-            }
-        }
-    }
-*/
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,59 +42,6 @@ public class NewWishActivity extends AppCompatActivity {
         restWishService = new RestWishService();
     }
 
-    /** Create a file Uri for saving an image or video */
-    /*private static Uri getOutputMediaFileUri(int type){
-        return Uri.fromFile(getOutputMediaFile(type));
-    }*/
-
-    /** Create a File for saving an image or video */
-  /*  private static File getOutputMediaFile(int type){
-        // To be safe, you should check that the SDCard is mounted
-        // using Environment.getExternalStorageState() before doing this.
-
-        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES), "MyCameraApp");
-        // This location works best if you want the created images to be shared
-        // between applications and persist after your app has been uninstalled.
-
-        // Create the storage directory if it does not exist
-        if (! mediaStorageDir.exists()){
-            if (! mediaStorageDir.mkdirs()){
-                Log.d("MyCameraApp", "failed to create directory");
-                return null;
-            }
-        }
-*/
-        // Create a media file name
-       /* String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        File mediaFile;
-        if (type == MEDIA_TYPE_IMAGE){
-            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
-                    "IMG_"+ timeStamp + ".jpg");
-        } else if(type == MEDIA_TYPE_VIDEO) {
-            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
-                    "VID_"+ timeStamp + ".mp4");
-        } else {
-            return null;
-        }
-
-        return mediaFile;
-    }*/
-
-    // Onclick take picture-button
-    public void takeAPicture(View view) {
-        /*// create Intent to take a picture and return control to the calling application
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-        fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE); // create a file to save the image
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
-
-        // start the image capture Intent
-        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-*/
-
-    }
-
     // Onclick register new wish-button
     public void registerNewWish(View view) {
         EditText nameEdit = (EditText) findViewById(R.id.wishName);
@@ -121,8 +49,6 @@ public class NewWishActivity extends AppCompatActivity {
         EditText whereEdit = (EditText) findViewById(R.id.wishWhere);
         EditText linkEdit = (EditText) findViewById(R.id.wishLink);
         EditText priceEdit = (EditText) findViewById(R.id.wishPrice);
-
-        //TODO: ImageView imageView = (ImageView) findViewById(R.id.wishImage);
 
         String name = nameEdit.getText().toString();
         String spesification = spesEdit.getText().toString();
@@ -158,7 +84,6 @@ public class NewWishActivity extends AppCompatActivity {
         wish.Link = link;
         wish.Price = price;
         wish.WishListId = wishListId;
-        // TODO: wish.Image = image;
         wish.Bought = false;
 
         // disable button click (in case it takes some time):
@@ -176,6 +101,7 @@ public class NewWishActivity extends AppCompatActivity {
                 toastView.setBackgroundResource(R.color.background_color);
                 toast.show();
 
+                // Go back
                 Intent i = new Intent(NewWishActivity.this, EditWishListActivity.class);
                 i.putExtra("WISHLISTID", wishListId);
                 i.putExtra("WISHLISTNAME", wishListName);
